a33ed686a9cf66a24dea5c6033b71c8bb8e2d6e7
Hayden Aiken <hayden@openaiken.net>
2024-06-25 19:10:13 -0500

    added requirements, and transitioning to ini instead of py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Configuration for bashbridge (bridge.py)
-#Minecraft Server IP is for the rcon connection, which is a loopback address if it's on the same host.
-MINECRAFT_SERVER_IP = '127.0.0.1'
-RCON_PORT = 25575
-RCON_PASSWORD = 'YourMinecraftServersRconPasswordFromServer.Properties'
-DISCORD_BOT_TOKEN = 'YourDiscordBotsTokenGoesInTheseQuotes'
-DISCORD_CHANNEL_ID = 12345678910
-MC_LOG_FILE_PATH = '/data/minecraft/yourserver/logs/latest.log'
-
